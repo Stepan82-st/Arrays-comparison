06358eed71b485ad0aeb4f3150ca70cc1780d485
Stepan82-st <bryl.step@tut.by>
2021-11-15 22:08:08 +0300

    Add files via upload
    The task is to compare the symbols of a multidimensional and one-dimensional array and display the coordinates of the location of the matching elements.

---
+++ index.js
@@ -0,0 +1,35 @@
+function getLetterCoordinates(matrix,letter){
+  const indexElementMatrix = [];
+  const letterArray = letter.split('');
+  let matrixArray = matrix.split('');
+  let n = Math.sqrt(matrixArray.length);
+  const result = [];
+  if (typeof matrix !== 'undefined' && typeof letter !== 'undefined')
+  while(matrixArray.length > 0)
+  { 
+  result.push(matrixArray.slice(0, n));
+  matrixArray = matrixArray.slice(n);
+  }
+  const result2 = [];
+  for(let i =0; i<= result.length; i++)
+  if (typeof result[i] !== 'undefined')
+  result[i].forEach(function(element){
+  letterArray.forEach(item => (item === element)?result2.push(element): 'ne to')
+  });
+
+if (n % 3 === 0){
+ for (let i = 0; i < result2.length; i++){
+      for (let y = 0; y < result.length; y++) {
+        for (let x = 0; x < result[0].length; x++) { 
+          if (result[y][x] == result2[i]) 
+          indexElementMatrix.push([y,x]);         
+        }
+      }
+ }
+ return indexElementMatrix;  
+}else {console.log('ne % 3, because ne to'); 
+}  
+}  
+  console.log(getLetterCoordinates('Stepanbry', 'bal'));
+
+
